218c478e13b1e23acf2ccd018fc443b1bf5f5a12
valvikanaiya <kanaiyawebdev0@gmail.com>
2024-10-09 21:58:23 +0530

    update

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -3,7 +3,7 @@ import SpeceMeet from "@assets/SpeceMeet.jpg";
 const Navigation = () => {
   return (
     <nav className="bg-[#4C4072] w-full p-2 flex items-center justify-between">
-      <img className="h-[50px] w-[60px]" src={Logo} alt="" />
+      <img className="h-[50px] w-[80px]" src={Logo} alt="" />
       <img className=" h-[50px] w-[60px]" src={SpeceMeet} alt="" />
     </nav>
   );
