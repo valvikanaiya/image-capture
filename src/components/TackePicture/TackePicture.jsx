87f75ba87cbf3006ccd5835394429600b9a51a58
valvikanaiya <kanaiyawebdev0@gmail.com>
2024-10-10 11:39:24 +0530

    fixed file change event

--- src/components/TackePicture/TackePicture.jsx
+++ src/components/TackePicture/TackePicture.jsx
@@ -39,7 +39,7 @@ const TackePicture = () => {
   }, [image]);
 
   const handleImageCapture = async (e) => {
-    const file = e.target.files[0];
+    const file = e.target.files?.[0];
 
     if (file) {
       const reader = new FileReader();
